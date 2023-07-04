266a6dc5af0847d183998b5019cc05f6e3dbe9af
unknowncz <86740586+unknowncz@users.noreply.github.com>
2023-07-04 12:09:46 +0200

    file upload

---
+++ output.py
@@ -0,0 +1,101 @@
+import os
+import subprocess
+
+SELFDIR = os.path.abspath(f'{__file__}/..')
+
+from src.OPi import OPiTools
+
+class Pin:
+    defaut_pin_info = {
+        'GPIO': -1,
+        'wPi': -1,
+        'Physical': -1,
+        'name': '',
+        'pinmode': None
+    }
+    def __init__(self, **pin_info) -> None:
+        pin_info = {**self.defaut_pin_info, **pin_info}
+        self.GPIO = pin_info['GPIO']
+        self.wPi = pin_info['wPi']
+        self.physical = pin_info['Physical']
+        self.name = pin_info['name']
+        self.pinmode = pin_info['pinmode']
+        if self.wPi == -1:
+            self.value = int(subprocess.check_output(f'gpio read {self.wPi}').strip())
+        else:
+            self.value = -1
+
+
+    def write(self, value:int):
+        self.value = value
+        subprocess.Popen(f'gpio write {self.wPi} {value}')
+
+    def read(self):
+        self.value = int(subprocess.check_output(f'gpio read {self.wPi}').strip())
+        return self.value
+
+class GPIOmgr:
+    def __init__(self, pins:list) -> None:
+        self.pins = pins
+        self.pinmap_physical = {pin.physical: pin for pin in pins}
+        self.pinmap_wPi = {pin.wPi: pin for pin in pins if pin.wPi != -1}
+        self.pinmap_GPIO = {pin.GPIO: pin for pin in pins if pin.GPIO != -1}
+
+    def digitalwrite(self, pin_wPi:int, value:int):
+        subprocess.Popen(f'gpio write {pin_wPi} {value}'.split())
+
+    def digitalread(self, pin_wPi:int):
+        return int(subprocess.check_output(f'gpio read {pin_wPi}').strip())
+
+    def phys2wPi(self, pin_physical:int):
+        return self.pinmap_physical[pin_physical].wPi
+
+    def phys2GPIO(self, pin_physical:int):
+        return self.pinmap_physical[pin_physical].GPIO
+
+    def wPi2phys(self, pin_wPi:int):
+        return self.pinmap_wPi[pin_wPi].physical
+
+    def wPi2GPIO(self, pin_wPi:int):
+        return self.pinmap_wPi[pin_wPi].GPIO
+
+    def GPIO2phys(self, pin_GPIO:int):
+        return self.pinmap_GPIO[pin_GPIO].physical
+
+    def GPIO2wPi(self, pin_GPIO:int):
+        return self.pinmap_GPIO[pin_GPIO].wPi
+
+    def isWriteable(self, pin_wPi:int):
+        output_pins = filter(lambda pin: pin.pinmode == OPiTools.OUTPUT, self.pins)
+        return pin_wPi in map(lambda pin: pin.wPi, output_pins)
+
+    def isReadable(self, pin_wPi:int):
+        # input pins are readable
+        input_pins = filter(lambda pin: pin.pinmode == OPiTools.INPUT or pin.pinmode == OPiTools.INPUT_PULLUP, self.pins)
+        return pin_wPi in map(lambda pin: pin.wPi, input_pins)
+
+    def setMode(self, pin_wPi:int, mode:int):
+        if mode == OPiTools.OUTPUT:
+            self.pinmap_wPi[pin_wPi].pinmode = OPiTools.OUTPUT
+        elif mode == OPiTools.INPUT:
+            self.pinmap_wPi[pin_wPi].pinmode = OPiTools.INPUT
+        elif mode == OPiTools.INPUT_PULLUP:
+            self.pinmap_wPi[pin_wPi].pinmode = OPiTools.INPUT_PULLUP
+        else:
+            raise ValueError(f'Invalid mode {mode}')
+
+
+if __name__ == '__main__':
+    import time
+    # create pin classes from PINLIST dict
+    pins = [Pin(**pin) for pin in OPiTools.PINLIST]
+    # create GPIOmgr class
+    GPIO = GPIOmgr(pins)
+    # set physical pin 7 to output
+    GPIO.setMode(GPIO.phys2wPi(7), OPiTools.OUTPUT)
+    # create a blink loop
+    while True:
+        GPIO.digitalwrite(GPIO.phys2wPi(7), OPiTools.HIGH)
+        time.sleep(1)
+        GPIO.digitalwrite(GPIO.phys2wPi(7), OPiTools.LOW)
+        time.sleep(1)
---
+++ src/OPi/OPiTools.py
@@ -0,0 +1,22 @@
+HIGH = 1
+LOW = 0
+
+OUTPUT = 0
+INPUT = 1
+INPUT_PULLUP = 2
+
+PINLIST = [
+    {'name': '3.3V', 'Physical': 1, 'wPi': -1, 'GPIO': -1, 'pinmode': None},            {'name': '5V', 'Physical': 2, 'wPi': -1, 'GPIO': -1, 'pinmode': None},
+    {'name': 'SDA.5', 'Physical': 3, 'wPi': 0, 'GPIO': 47, 'pinmode': INPUT},           {'name': '5V', 'Physical': 4, 'wPi': -1, 'GPIO': -1, 'pinmode': None},
+    {'name': 'SCL.5', 'Physical': 5, 'wPi': 1, 'GPIO': 46, 'pinmode': INPUT},           {'name': 'GND', 'Physical': 6, 'wPi': -1, 'GPIO': -1, 'pinmode': None},
+    {'name': 'PWM15', 'Physical': 7, 'wPi': 2, 'GPIO': 54, 'pinmode': INPUT},           {'name': 'RXD.0', 'Physical': 8, 'wPi': 3, 'GPIO': 131, 'pinmode': INPUT},
+    {'name': 'GND', 'Physical': 9, 'wPi': -1, 'GPIO': -1, 'pinmode': None},             {'name': 'TXD.0', 'Physical': 10, 'wPi': 4, 'GPIO': 132, 'pinmode': INPUT},
+    {'name': 'CAN1_RX', 'Physical': 11, 'wPi': 5, 'GPIO': 138, 'pinmode': INPUT},       {'name': 'CAN2_TX', 'Physical': 12, 'wPi': 6, 'GPIO': 29, 'pinmode': INPUT},
+    {'name': 'CAN1_TX', 'Physical': 13, 'wPi': 7, 'GPIO': 139, 'pinmode': INPUT},       {'name': 'GND', 'Physical': 14, 'wPi': -1, 'GPIO': -1, 'pinmode': None},
+    {'name': 'CAN2_RX', 'Physical': 15, 'wPi': 8, 'GPIO': 28, 'pinmode': INPUT},        {'name': 'SDA.1', 'Physical': 16, 'wPi': 9, 'GPIO': 59, 'pinmode': INPUT},
+    {'name': '3.3V', 'Physical': 17, 'wPi': -1, 'GPIO': -1, 'pinmode': None},           {'name': 'SCL.1', 'Physical': 18, 'wPi': 10, 'GPIO': 58, 'pinmode': INPUT},
+    {'name': 'SPI4_TXD', 'Physical': 19, 'wPi': 11, 'GPIO': 49, 'pinmode': INPUT},      {'name': 'GND', 'Physical': 20, 'wPi': -1, 'GPIO': -1, 'pinmode': None},
+    {'name': 'SPI4_RXD', 'Physical': 21, 'wPi': 12, 'GPIO': 48, 'pinmode': INPUT},      {'name': 'PowerKey', 'Physical': 22, 'wPi': -1, 'GPIO': -1, 'pinmode': None},
+    {'name': 'SPI4_CLK', 'Physical': 23, 'wPi': 13, 'GPIO': 50, 'pinmode': INPUT},      {'name': 'SPI4_CS1', 'Physical': 24, 'wPi': 14, 'GPIO': 52, 'pinmode': INPUT},
+    {'name': 'GND', 'Physical': 25, 'wPi': -1, 'GPIO': -1, 'pinmode': None},            {'name': 'PWM1', 'Physical': 26, 'wPi': 15, 'GPIO': 35, 'pinmode': INPUT},
+]
